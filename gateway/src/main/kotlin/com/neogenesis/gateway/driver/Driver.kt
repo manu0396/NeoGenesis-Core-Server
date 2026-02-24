b3e7aa9ef63a8397fcbb0d1c91ec9425036c06a3
manu0396 <manulucas0396@gmail.com>
2026-02-24 19:10:00 +0100

    feat: add gateway driver plugin

---
+++ gateway/src/main/kotlin/com/neogenesis/gateway/driver/Driver.kt
@@ -0,0 +1,29 @@
+﻿package com.neogenesis.gateway.driver
+
+import kotlinx.coroutines.flow.Flow
+
+interface Driver {
+    val name: String
+
+    fun telemetryStream(): Flow<DriverTelemetry>
+
+    fun eventStream(): Flow<DriverRunEvent>
+
+    fun close()
+}
+
+data class DriverTelemetry(
+    val runId: String,
+    val metricKey: String,
+    val metricValue: Double,
+    val unit: String,
+    val driftScore: Double,
+    val recordedAtMs: Long,
+)
+
+data class DriverRunEvent(
+    val runId: String,
+    val eventType: String,
+    val payloadJson: String,
+    val createdAtMs: Long,
+)
---
+++ gateway/src/main/kotlin/com/neogenesis/gateway/driver/DriverRegistry.kt
@@ -0,0 +1,10 @@
+﻿package com.neogenesis.gateway.driver
+
+class DriverRegistry {
+    fun create(name: String): Driver {
+        return when (name.lowercase()) {
+            "simulated" -> SimulatedDriver(runId = "run-sim")
+            else -> error("Unknown driver: $name")
+        }
+    }
+}
