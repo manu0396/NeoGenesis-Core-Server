2740eb4a165b850fa370b1b50749d9b64a4461c9
manu0396 <manulucas0396@gmail.com>
2026-03-01 12:00:00 +0100

    feat: add gateway offline queue

---
+++ gateway/src/main/kotlin/com/neogenesis/gateway/BackpressureLimiter.kt
@@ -0,0 +1,32 @@
+﻿package com.neogenesis.gateway
+
+import java.util.concurrent.Semaphore
+import java.util.concurrent.TimeUnit
+
+class BackpressureLimiter(
+    maxPerSecond: Int,
+) {
+    private val permits = Semaphore(maxPerSecond, true)
+    private val refillThread =
+        Thread {
+            while (true) {
+                val missing = maxPerSecond - permits.availablePermits()
+                if (missing > 0) {
+                    permits.release(missing)
+                }
+                Thread.sleep(1000)
+            }
+        }.apply {
+            isDaemon = true
+            name = "gateway-backpressure"
+            start()
+        }
+
+    fun acquire(batchSize: Int, timeoutMs: Long): Boolean {
+        return permits.tryAcquire(batchSize, timeoutMs, TimeUnit.MILLISECONDS)
+    }
+
+    fun stop() {
+        refillThread.interrupt()
+    }
+}
